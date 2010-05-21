5b21a3e41276cb0e8e0a5c312aabf4b3c4e9fdd3
drivehappy <drivehappy@7af81de8-dd64-11de-95c9-073ad895b44c>
2010-05-21 07:32:59 +0000

    Added helpers for retrieving item stack info

--- Source/CEquipment.h
+++ Source/CEquipment.h
@@ -5,8 +5,10 @@
 struct CEquipment
 {
   // CItem STRUCTURE HERE
+  u32 stackSize;    // 13h = 19 pots
+  u32 stackSizeMax; // 14h = 20 max size pots
 
-  u32 unk1000[23];  //
+  u32 unk1000[21];  //
 
   PVOID pCGenericModel;
 
