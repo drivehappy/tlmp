0e3b04a99dfbfc5801817f2191499c7077d04e20
drivehappy <drivehappy@7af81de8-dd64-11de-95c9-073ad895b44c>
2010-09-04 17:40:30 +0000

    Fixed client bug not sending Server character items on level change. BUG: First Aldric Cinematic locks the client player in cinematic mode. BUG: Client doesn't properly receive "Real" equipment enchants when created on server. BUG: Client characters created are stuck in pile.

--- src/Network/SharedState.cpp
+++ src/Network/SharedState.cpp
@@ -7,6 +7,19 @@ vector<NetworkEntity*> *TLMP::ClientTemporaryEquipment = new vector<NetworkEntit
 map<SystemAddress, vector<NetworkEntity*>*> *TLMP::Server_ClientCharacterMapping = new map<SystemAddress, vector<NetworkEntity*>*>();
 vector<NetworkEntity*> *TLMP::ServerEquipmentOnGround = new vector<NetworkEntity*>();
 vector<NetworkEntity*> *TLMP::NetworkSharedLevelItems = new vector<NetworkEntity*>();
+vector<CBaseUnit*>     *TLMP::OutsideBaseUnits = new vector<CBaseUnit*>();
 
-vector<CBaseUnit*> *TLMP::OutsideBaseUnits = new vector<CBaseUnit*>();
+void TLMP::clearAllNetworkIDs()
+{
+  if (TLMP::Network::NetworkState::getSingleton().GetState() == TLMP::Network::CLIENT) {
+    // Commented this because server doesn't recreate it's characters' inventory on level changes
+    NetworkSharedEquipment->clear();
+  }
 
+  NetworkSharedCharacters->clear();
+  ClientTemporaryEquipment->clear();
+  Server_ClientCharacterMapping->clear();
+  ServerEquipmentOnGround->clear();
+  OutsideBaseUnits->clear();
+  NetworkSharedLevelItems->clear();
+}
